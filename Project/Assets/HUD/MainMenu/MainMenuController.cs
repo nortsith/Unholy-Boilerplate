097b259622a17d8dc8294ea78c22ff8f889dca42
Onur Şahin Şentürk <onurssx9@gmail.com>
2022-10-11 17:29:57 +0300

    Video and Quality Settings

--- Project/Assets/HUD/MainMenu/MainMenuController.cs
+++ Project/Assets/HUD/MainMenu/MainMenuController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Linq;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -20,6 +21,14 @@ public class MainMenuController : MonoBehaviour
     private Slider musicSlider;
     private Slider environmentSlider;
     private Slider effectsSlider;
+
+    Resolution[] resolutions;
+    List<string> refreshRates;
+    private DropdownField modeDropdown;
+    private DropdownField resolutionDropdown;
+    private DropdownField refreshRateDropdown;
+
+    private DropdownField qualityDropdown;
 
     // Start is called before the first frame update
     void Start()
@@ -52,6 +61,99 @@ public class MainMenuController : MonoBehaviour
         musicSlider.RegisterValueChangedCallback(value => musicVcaController.setVolume(value.newValue));
         environmentSlider.RegisterValueChangedCallback(value => environmentVcaController.setVolume(value.newValue));
         effectsSlider.RegisterValueChangedCallback(value => effectsVcaController.setVolume(value.newValue));
+
+        modeDropdown = root.Q<DropdownField>("ModeDropdown");
+        resolutionDropdown = root.Q<DropdownField>("ResolutionDropdown");
+        refreshRateDropdown = root.Q<DropdownField>("RefreshRateDropdown");
+        PopulateModes();
+        PopulateResolutions();
+        PopulateRefreshRates();
+
+        qualityDropdown = root.Q<DropdownField>("QualityDropdown");
+        PopulateQualityOptions();
+    }
+
+    private void PopulateModes()
+    {
+        modeDropdown.choices = new List<string>() { "Fullscreen", "Borderless", "Windowed" };
+        modeDropdown.index = 0;
+
+        modeDropdown.RegisterValueChangedCallback(value => SetMode());
+    }
+
+    private void SetMode()
+    {
+        switch (modeDropdown.value)
+        {
+            case "Fullscreen":
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case "Borderless":
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                break;
+            case "Windowed":
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                break;
+        }
+    }
+
+    private void PopulateResolutions()
+    {
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int index = 0; index < resolutions.Length; index++)
+        {
+            string option = resolutions[index].width + "x" + resolutions[index].height;
+            options.Add(option);
+
+            if (resolutions[index].width == Screen.currentResolution.width &&
+                resolutions[index].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = index;
+            }
+        }
+
+        resolutionDropdown.choices = options;
+        resolutionDropdown.index = currentResolutionIndex;
+
+        resolutionDropdown.RegisterValueChangedCallback(value => SetResolution());
+    }
+
+    private void SetResolution()
+    {
+        Resolution resolution = resolutions[resolutionDropdown.index];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    private void PopulateRefreshRates()
+    {
+        refreshRates = new List<string>() { "Uncapped", "60", "144", "240" };
+        int currentRefreshRateIndex = 0;
+        refreshRateDropdown.choices = refreshRates;
+        refreshRateDropdown.index = currentRefreshRateIndex;
+        refreshRateDropdown.RegisterValueChangedCallback(value => SetRefreshRate());
+    }
+
+    private void SetRefreshRate()
+    {
+        if (refreshRateDropdown.value == "Uncapped")
+            Application.targetFrameRate = -1;
+        else
+            Application.targetFrameRate = int.Parse(refreshRateDropdown.value);
+    }
+
+    private void PopulateQualityOptions()
+    {
+        qualityDropdown.choices = new List<string>() { "Very Low", "Low", "Medium", "High", "Very High", "Ultra" };
+        qualityDropdown.RegisterValueChangedCallback(value => SetQuailty());
+        qualityDropdown.index = QualitySettings.GetQualityLevel();
+    }
+
+    private void SetQuailty()
+    {
+        QualitySettings.SetQualityLevel(qualityDropdown.index);
     }
 
     // Update is called once per frame
